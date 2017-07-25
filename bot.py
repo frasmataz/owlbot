c1e9f1336707192c7c0f0076d97fd554441e9927
Fraser Sharp <fraserrsharp@gmail.com>
2017-07-25 16:49:38 +0100

    light refactoring

--- bot.py
+++ bot.py
@@ -27,13 +27,15 @@ async def on_message(message):
         if role.name == 'admin':
             adminrole = role
 
+    username = message.author.name
+
     if message.content.startswith('!droneweather'):
         await client.send_message(message.channel, get_weather())
     else:
-        if not message.author.name in users:
-            users[message.author.name] = {
-                "linkedmessages": 0,
-                "unlinkedmessages": 0,
+        if not username in users:
+            users[username] = {
+                "links": 0,
+                "not-links": 0,
                 "linksinarow": 0
             }
 
@@ -41,38 +43,43 @@ async def on_message(message):
 
         if message.content.startswith("!rep"):
             await client.send_message(message.channel, "You're sitting at " +
-                str(users[message.author.name]["linkedmessages"]) + " links, and " +
-                str(users[message.author.name]["unlinkedmessages"]) + " not-links.")
+                str(users[username]["links"]) + " links, and " +
+                str(users[username]["not-links"]) + " not-links.")
         elif message.content.startswith("!reset") and adminrole in message.author.roles:
             users = {}
         else:
             if not messagelinks:
-                users[message.author.name]["unlinkedmessages"] += 1
-                users[message.author.name]["linksinarow"] = 0
-                users[message.author.name]["warned"] = False
+                users[username]["not-links"] += 1
+                users[username]["linksinarow"] = 0
+                users[username]["warned"] = False
             else:
-                users[message.author.name]["linkedmessages"] += 1
-                users[message.author.name]["linksinarow"] += 1
-                if (users[message.author.name]["unlinkedmessages"] > 0
-                and (users[message.author.name]["linkedmessages"] / users[message.author.name]["unlinkedmessages"] > warnlinkratio)):
-                    if (users[message.author.name]["linkedmessages"] / users[message.author.name]["unlinkedmessages"] > maxlinkratio):
-                        if users[message.author.name]["linksinarow"] == 2:
+                users[username]["links"] += 1
+                users[username]["linksinarow"] += 1
+
+                if (users[username]["not-links"] > 0
+                and (users[username]["links"] / users[username]["not-links"] > warnlinkratio)):
+                    if (users[username]["links"] / users[username]["not-links"] > maxlinkratio):
+                        if users[username]["linksinarow"] == 2:
                             await client.send_message(message.channel, message.author.mention+" ALL YOU DO IS LINK, STAHP.  REEEEEEEEEEE " + adminrole.mention)
-                        elif users[message.author.name]["linksinarow"] >= 3:
-                            if users[message.author.name]["warned"] == True:
+
+                        elif users[username]["linksinarow"] >= 3:
+                            if users[username]["warned"] == True:
                                 await client.send_message(message.channel, "kickin fuck outta " + message.author.mention)
                                 await client.kick(message.author)
                                 await client.send_message(message.channel, "so say we all")
+
                             else:
                                 await client.send_message(message.channel, message.author.mention+", "+adminrole.mention+" arming kick button, you've been warned")
-                                users[message.author.name]["warned"] = True
+                                users[username]["warned"] = True
+
                     else:
-                        if users[message.author.name]["linksinarow"] == 2:
+                        if users[username]["linksinarow"] == 2:
                             await client.send_message(message.channel, "Hey "+message.author.mention+", watch that link spam boyo")
-                        elif users[message.author.name]["linksinarow"] >= 3:
+
+                        elif users[username]["linksinarow"] >= 3:
                             await client.send_message(message.channel, message.author.mention+" STAHP")
 
-                users[message.author.name]["lastmessage"] = "linked"
+                users[username]["lastmessage"] = "linked"
 
 def get_weather():
     forecastdayname = calendar.day_name[(datetime.datetime.now() + datetime.timedelta(days=2)).weekday()]
